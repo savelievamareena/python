f95e1798bdd7397123c0898b6ca648c61899ec9e
savelievamareena <savelieva.mareena.1.21@gmail.com>
2022-05-22 14:40:50 +0200

    задача на одномерный список

---
+++ lab2/task2.py
@@ -0,0 +1,21 @@
+#Введите одномерный целочисленный список. Найдите наибольший нечетный элемент.
+#Найдите минимальный по модулю элемент списка.
+
+from random import *
+
+A = [randint(-1000, 1000) for _ in range(20)]
+print(A)
+
+odds = []
+mod = []
+for i in range(0, len(A) - 1):
+    if A[i] % 2 == 1:
+        odds.append(A[i])
+    modulo = A[i] if A[i] > 0 else A[i] * -1
+    mod.append(modulo)
+
+print("-----")
+print(odds)
+print(mod)
+print(f"Максимальное нечетное число: {max(odds)}")
+print(f"Максимальное число по модулю: {max(mod)}")
