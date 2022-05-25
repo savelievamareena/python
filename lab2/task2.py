2b4ff41270ca80d2077118e47df14618c96051f2
savelievamareena <savelieva.mareena.1.21@gmail.com>
2022-05-25 14:43:51 +0200

    добавлено второе решение

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,6 +1,5 @@
-#Введите одномерный целочисленный список. Найдите наибольший нечетный элемент.
-#Найдите минимальный по модулю элемент списка.
-
+# Введите одномерный целочисленный список. Найдите наибольший нечетный элемент.
+# Найдите минимальный по модулю элемент списка.
 from random import *
 
 A = [randint(-1000, 1000) for _ in range(20)]
@@ -14,8 +13,25 @@ for i in range(0, len(A) - 1):
     modul = A[i] if A[i] > 0 else A[i] * -1
     mod.append(modul)
 
-print("-----")
-print(odds)
-print(mod)
 print(f"Максимальное нечетное число: {max(odds)}")
-print(f"Максимальное число по модулю: {max(mod)}")
+print(f"Минимальное число по модулю: {min(mod)}")
+
+# решение через фильтр и мапу
+
+
+def odds_fun(num):
+    return True if num % 2 == 1 else False
+
+
+def mod_fun(num):
+    return abs(num)
+
+
+A = [randint(-1000, 1000) for _ in range(20)]
+print(A)
+
+odds = list(filter(odds_fun, A))
+mod = list(map(mod_fun, A))
+
+print(f"Максимальное нечетное число: {max(odds)}")
+print(f"Минимальное число по модулю: {min(mod)}")
