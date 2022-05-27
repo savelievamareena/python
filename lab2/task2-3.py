c489c791db6d9d976af6620ea967d534e142b40a
savelievamareena <savelieva.mareena.1.21@gmail.com>
2022-05-27 13:14:26 +0200

    добавлена вторая задача из второго раздела

--- lab2/task2-3.py
+++ lab2/task2-3.py
@@ -17,3 +17,21 @@ for i in range(n):
         matrix3[i][j] = matrix1[i][j] + matrix2[i][j]
 
 print(matrix3)
+print("---------")
+
+#1.В матрице найти номер строки, сумма чисел в которой максимальна.
+
+matrix0 = [[randrange(1, 11) for _ in range(n)] for _ in range(n)]
+print(matrix0)
+
+max_row_sum = 0
+row_num = 0
+for i in range(n):
+    temp_sum = 0
+    for j in range(n):
+        temp_sum += matrix0[i][j]
+    if temp_sum > max_row_sum:
+        max_row_sum = temp_sum
+        row_num = i + 1
+
+print(f"В строке под номером {row_num} сумма чисел максимальна и равняется {max_row_sum}")
