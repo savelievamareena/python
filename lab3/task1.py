a941058e43735121a120ebe9348258c98063fc39
savelievamareena <savelieva.mareena.1.21@gmail.com>
2022-05-31 17:33:45 +0200

    добавлена вторая сортировка

--- lab3/task1.py
+++ lab3/task1.py
@@ -1,4 +1,4 @@
-# сортировка
+# Функция сортировки вставками insert
 from random import *
 
 N = int(input("Введите размер списка: "))
@@ -9,11 +9,30 @@ for i in range(1, len(A)):
     t = A[i]
     j = i
     for j in range(i, 0, -1):
-        if A[j-1] > t:
-            A[j] = A[j-1]
+        if A[j - 1] > t:
+            A[j] = A[j - 1]
             j -= 1
         else:
             break
     A[j] = t
 
 print(A)
+
+# Функция шейкерной (коктейльной) сортировки shaker
+
+B = [randint(-100, 100) for _ in range(N)]
+print(B)
+
+for i in range(0, int(N / 2)):
+    for j in range(i, N - 1 - i):
+        if B[j] > B[j + 1]:
+            temp = B[j]
+            B[j] = B[j + 1]
+            B[j + 1] = temp
+    for j in range(N - 2 - i, i, -1): #если до i+1 как в условии то самый первый элемент не отсортирован*, работает правильно когда range до i
+        if B[j] < B[j - 1]:
+            temp = B[j]
+            B[j] = B[j - 1]
+            B[j - 1] = temp
+
+print(B)
