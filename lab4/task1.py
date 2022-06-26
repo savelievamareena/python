1dbd34c975e9735f8b546101343d73b810ff60ce
savelievamareena <savelieva.mareena.1.21@gmail.com>
2022-06-26 12:39:26 +0200

    lab4

--- lab4/task1.py
+++ lab4/task1.py
@@ -6,14 +6,14 @@
 
 class House:
     house_list = []
-    _house_id_ = 0
-    _apt_ = 0
-    _square_ = 0
-    _floor_ = 0
-    _rooms_num_ = 0
-    _street_ = ""
-    _building_type_ = ""
-    _lifetime_ = 0
+    _house_id = 0
+    _apt = 0
+    _square = 0
+    _floor = 0
+    _rooms_num = 0
+    _street = ""
+    _building_type = ""
+    _lifetime = 0
 
     def __init__(self, house_id, apt, square, floor, rooms_num, street, building_type, lifetime):
         self.set_house_id(house_id)
@@ -24,82 +24,77 @@ class House:
         self.set_street = street
         self.set_building_type = building_type
         self.set_lifetime = lifetime
-        self.house_list.append(self)
 
     @classmethod
     def create_default_house(cls, house_id, apt, square, floor, rooms_num):
         default_house = cls(house_id, apt, square, floor, rooms_num, 'Slobodskaya', 'panel house', 70)
         return default_house
 
-    @staticmethod
-    def function_a(house_list, number_of_rooms):
+    def function_a(self, house_list, number_of_rooms):
         result = []
         for house in house_list:
-            if number_of_rooms == house.__rooms_num:
+            if number_of_rooms == self.get_rooms_num():
                 result.append(house)
         return result
 
     def function_b(self, number_of_rooms, min_floor, max_floor):
         result = []
         for house in self.house_list:
-            if number_of_rooms == house.__rooms_num and min_floor <= house.__floor <= max_floor:
+            if number_of_rooms == self.get_rooms_num() and min_floor <= self.get_floor() <= max_floor:
                 result.append(house)
         return result
 
     def get_house_id(self):
-        return self._house_id_
+        return self._house_id
 
     def set_house_id(self, house_id):
-        self._house_id_ = house_id
+        self._house_id = house_id
 
     def get_apt(self):
-        return self._apt_
+        return self._apt
 
     def set_apt(self, apt):
-        self._apt_ = apt
+        self._apt = apt
 
     def get_square(self):
-        return self._square_
+        return self._square
 
     def set_square(self, square):
-        self._square_ = square
+        self._square = square
 
     def get_floor(self):
-        return self._floor_
+        return self._floor
 
     def set_floor(self, floor):
-        self._floor_ = floor
+        self._floor = floor
 
     def get_rooms_num(self):
-        return self._rooms_num_
+        return self._rooms_num
 
     def set_rooms_num(self, rooms_num):
-        self._rooms_num_ = rooms_num
+        self._rooms_num = rooms_num
 
     def get_street(self):
-        return self._street_
+        return self._street
 
     def set_street(self, street):
-        self._street_ = street
+        self._street = street
 
     def get_building_type(self):
-        return self._building_type_
+        return self._building_type
 
     def set_building_type(self, building_type):
-        self._building_type_ = building_type
+        self._building_type = building_type
 
     def get_lifetime(self):
-        return self._lifetime_
+        return self._lifetime
 
     def set_lifetime(self, lifetime):
-        self._lifetime_ = lifetime
+        self._lifetime = lifetime
 
 
-my_house_id = int(input('Enter House Id: '))
-my_apt = int(input('Enter Apartment Number: '))
-my_square = int(input('Enter Square: '))
-my_floor = int(input('Enter Floor: '))
-my_rooms_num = int(input('Enter Room: '))
+house1 = House.create_default_house(int(input('Enter House Id: ')), int(input('Enter Apartment Number: ')), int(input('Enter Square: ')), int(input('Enter Floor: ')), int(input('Enter Room: ')))
+house1 = House.create_default_house(int(input('Enter House Id: ')), int(input('Enter Apartment Number: ')), int(input('Enter Square: ')), int(input('Enter Floor: ')), int(input('Enter Room: ')))
+house1 = House.create_default_house(int(input('Enter House Id: ')), int(input('Enter Apartment Number: ')), int(input('Enter Square: ')), int(input('Enter Floor: ')), int(input('Enter Room: ')))
 
-house1 = House.create_default_house(my_house_id, my_apt, my_square, my_floor, my_rooms_num)
 print(house1)
