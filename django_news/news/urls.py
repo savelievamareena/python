6dc9158cc46e41201dee58db1b4ed2c84e25162f
savelievamareena <savelieva.mareena.1.21@gmail.com>
2022-09-19 20:58:06 +0300

    Bootstrap + urls

--- django_news/news/urls.py
+++ django_news/news/urls.py
@@ -6,4 +6,8 @@ app_name = 'riddles'
 urlpatterns = [
     path(r'', views.index, name='index'),
     re_path(r'^([0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^register/$', views.RegisterFormView.as_view()),
+    re_path(r'^login/$', views.LoginFormView.as_view()),
+    re_path(r'^logout/$', views.LogoutView.as_view()),
+    re_path(r'^password-change/', views.PasswordChangeView.as_view()),
 ]
