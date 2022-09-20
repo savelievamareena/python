d656c381f6fb5ca4f16beb97123167db0fd2027d
savelievamareena <savelieva.mareena.1.21@gmail.com>
2022-09-20 21:50:19 +0300

    Refactoring + new functionality (admin panel)

--- django_news/news/models.py
+++ django_news/news/models.py
@@ -1,11 +1,34 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 from django.db import models
 
 
-class Title(models.Model):
-    title_text = models.CharField(max_length=255)
+# class Title(models.Model):
+#     title_text = models.CharField(max_length=255)
+#     pub_date = models.DateTimeField('date published')
+#
+#
+# class NewsBody(models.Model):
+#     title = models.ForeignKey(Title, on_delete=models.CASCADE)
+#     text = models.TextField(blank=False)
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
     pub_date = models.DateTimeField('date published')
-
-
-class NewsBody(models.Model):
-    title = models.ForeignKey(Title, on_delete=models.CASCADE)
     text = models.TextField(blank=False)
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(
+        News,
+        verbose_name='Чат под новостью',
+        on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User,
+        verbose_name='Пользователь', on_delete=models.CASCADE)
+    message = models.TextField('Сообщение')
+    pub_date = models.DateTimeField(
+        'Дата сообщения',
+        default=timezone.now)
--- django_news/news/urls.py
+++ django_news/news/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     re_path(r'^login/$', views.LoginFormView.as_view()),
     re_path(r'^logout/$', views.LogoutView.as_view()),
     re_path(r'^password-change/', views.PasswordChangeView.as_view()),
+    re_path(r'^([0-9]+)/post/$', views.post, name='post'),
+    re_path(r'^admin/$', views.admin, name='admin'),
+    re_path(r'^post_news/$', views.post_news, name='post_news'),
 ]
